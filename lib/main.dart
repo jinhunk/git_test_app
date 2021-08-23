1dd1305806f47bbe800f530dc4a6f12cc6a8881c
jinhunk <chlwlsgur23@naver.com>
2021-08-23 21:57:57 +0900

    ㅁㄴㅇㄴㅁㅇㄴㅇ

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter git test 중',
+      title: 'Flutter git test 제발',
       theme: ThemeData(
         // This is the theme of your application.
         //
